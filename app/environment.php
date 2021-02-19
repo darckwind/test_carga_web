a43ce990ff8edefa8fe0d34dd628eba0efadb2ed
darckwind <sophie@MacBook-Pro-de-sophie.local>
2021-02-19 17:00:08 -0300

    feature: model load_test added with relationship

--- app/environment.php
+++ app/environment.php
@@ -7,12 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class environment extends Model
 {
     protected $fillable = [
-        'env_id','env_machine_name','env_machine_os','env_thread','env_ram','env_server'
+        'env_id','env_machine_name','env_machine_os','env_thread','env_ram','env_server','user_id'
     ];
     protected $primaryKey = 'env_id';
 
     public function user(){
         return $this->hasOne('App\User','id');
+    }
+
+    public function load_test(){
+        return $this->hasMany('App\load_test','load_test_id');
     }
 
 }
